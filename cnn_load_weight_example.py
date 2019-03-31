1eb2b5493f171cad65d1e5cf81cd32b59ca4178d
Giorgi Tsintsadze <44754948+hashtala@users.noreply.github.com>
2019-03-31 13:11:01 +0400

    Add files via upload

--- cnn_load_weight_example.py
+++ cnn_load_weight_example.py
@@ -11,7 +11,8 @@ Created on Thu Mar 21 21:47:24 2019
 '''
 
 
-VERY EASILY READABLE CODE
+SO IF YOU ARE NOT ME I AM NOT SURE IF YOU CAN READ THE CODE...
+
 
 '''
 
@@ -22,10 +23,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import theano.tensor as T
 
-data = np.array(pd.read_csv('train.csv')).astype(np.float32)
+data = np.array(pd.read_csv('digits.csv')).astype(np.float32)
 
-Y_train = data[:1000,0].astype(np.int32)
-X_train = data[:1000, 1:]
+Y_train = data[:1000,1].astype(np.int32)
+X_train = data[:1000, 2:]
 
 X_reshaped = np.float32(X_train.reshape(1000, 1, 28, 28)/255)
 
@@ -34,18 +35,22 @@ plt.imshow(X_reshaped[3, 0, :, :], cmap = 'Greys')
 plt.show()
 '''
 
-gela = cnn(CNN = [((5, 1, 5, 5), T.nnet.relu, (2,2)),
-                  ((10, 5, 5, 5), T.nnet.relu, (2,2))
+gela = cnn(CNN = [((30, 1, 5, 5), T.nnet.relu, (2,2)),
+                  ((10, 30, 5, 5), T.nnet.relu, (2,2))
                  ],
-           ANN = [(('x', 700), T.nnet.relu),
-                  ((700, 500), T.nnet.relu),
-                  ((500, 10), T.nnet.softmax)],
+           ANN = [(('x', 800), T.nnet.relu),
+                  ((800, 800), T.nnet.relu),
+                  ((800, 10), T.nnet.softmax)],
            shape_x = 28)
 
 
 gela.load_from_files()
-pred = gela.predict(X = X_reshaped, Y=  Y_train)
 
+
+
+pred = gela.predict(X = X_reshaped, Y=  Y_train)
+plt.imshow(X_reshaped[np.random.randint(0,1000), 0, :, :])
+plt.show()
 
 '''
 
